84c0bc02ba79664c020b1fbcbc97b6c6779db161
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-30 15:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateWindowSize() {
+      const widthSpan = document.getElementById('width');
+      const heightSpan = document.getElementById('height');
+      
+      widthSpan.textContent = window.innerWidth;
+      heightSpan.textContent = window.innerHeight;
+    }
+
+    // Initial update
+    updateWindowSize();
+
+    // Update on window resize
+    window.addEventListener('resize', updateWindowSize);
